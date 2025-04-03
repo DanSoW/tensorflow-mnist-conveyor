30da7d5ad455cbe58c0b281a0a504acab19fda23
DanSoW <swdaniel@yandex.ru>
2025-04-03 18:21:00 +0800

    update

--- mnist_pipeline_native_keras.py
+++ mnist_pipeline_native_keras.py
@@ -45,5 +45,96 @@ _beam_pipeline_args = [
         '--direct_num_workers=0',
 ]
 
+# Создание пайплайна с определёнными параметрами для классификации рукописных цифр MNIST
+def _create_pipeline(pipeline_name: str, pipeline_root: str, data_root: str,
+                     module_file: str, serving_model_dir: str, metadata_path: str,
+                     beam_pipeline_args: List[str], accuracy_threshold: float = 0.8) -> pipeline.Pipeline:
+    
+    # Импорт данных в конвейер
+    example_gen = ImportExampleGen(input_base=data_root)
+
+    # Вычисляет статистику по данным для визуализации и проверки на примере
+    statistics_gen = StatisticsGen(examples=example_gen.outputs['examples'])
+
+    # Генерация схема на основе файлов статистики
+    schema_gen = SchemaGen(
+            statistics=statistics_gen.outputs['statistics'], infer_feature_shape=True)
+
+    # Выполняет обнаружение аномалий на основе статистики и схемы данных
+    example_validator = ExampleValidator(
+            statistics=statistics_gen.outputs['statistics'],
+            schema=schema_gen.outputs['schema'])
+
+    # Преобразование данных
+    transform = Transform(
+            examples=example_gen.outputs['examples'],
+            schema=schema_gen.outputs['schema'],
+            module_file=module_file)
+    
+    # Создание компонента Trainer
+    def _create_trainer(module_file, component_id):
+        return Trainer(
+                module_file=module_file,
+                examples=transform.outputs['transformed_examples'],
+                transform_graph=transform.outputs['transform_graph'],
+                schema=schema_gen.outputs['schema'],
+                train_args=trainer_pb2.TrainArgs(num_steps=5000),
+                eval_args=trainer_pb2.EvalArgs(num_steps=100)).with_id(component_id)
+
+    trainer = _create_trainer(module_file, 'Trainer.mnist')
+    
+    # Конфигурация для оценки качества модели-кандидата
+    eval_config = tfma.EvalConfig(
+            model_specs=[tfma.ModelSpec(label_key='image_class')],
+            slicing_specs=[tfma.SlicingSpec()],
+            metrics_specs=[
+                tfma.MetricsSpec(metrics=[
+                    tfma.MetricConfig(
+                        class_name='SparseCategoricalAccuracy',
+                        threshold=tfma.MetricThreshold(
+                            value_threshold=tfma.GenericValueThreshold(
+                                lower_bound={'value': accuracy_threshold})))
+                ])
+            ])
+
+    # Использует TFMA для вычисления статистики оценки характеристик модели.
+    evaluator = Evaluator(
+            examples=example_gen.outputs['examples'],
+            model=trainer.outputs['model'],
+            eval_config=eval_config).with_id('Evaluator.mnist')
+
+    # Проверяет, прошла ли модель этапы проверки, и отправляет модель
+    # в пункт назначения файла, если проверка пройдена.
+    pusher = Pusher(
+            model=trainer.outputs['model'],
+            model_blessing=evaluator.outputs['blessing'],
+            push_destination=pusher_pb2.PushDestination(
+                filesystem=pusher_pb2.PushDestination.Filesystem(
+                    base_directory=serving_model_dir))).with_id('Pusher.mnist')
+
+    
+    return pipeline.Pipeline(
+            pipeline_name=pipeline_name,
+            pipeline_root=pipeline_root,
+            components=[
+                example_gen,
+                statistics_gen,
+                shema_gen,
+                example_validator,
+                transform,
+                trainer,
+                evaluator,
+                pusher,
+            ],
+            enable_cache=True,
+            metadata_connection_config=metadata.sqlite_metadata_connection_config(
+                metadata_path),
+            beam_pipeline_args=beam_pipeline_args)
+
+
+    
+
+
+
 
 
