e6191a4bc54b6bc68f5645a0620fde76acd5be39
DanSoW <swdaniel@yandex.ru>
2025-04-07 00:25:41 +0800

    update

--- mnist_utils_native_keras.py
+++ mnist_utils_native_keras.py
@@ -16,20 +16,9 @@ def _get_serve_tf_examples_fn(model, tf_transform_output):
     @tf.function
     def serve_tf_examples_fn(serialized_tf_examples):
         feature_spec = tf_transform_output.raw_feature_spec()
-
-        print("feature_spec: ", feature_spec)
-        print("serialized_tf_examples: ", serialized_tf_examples)
-
         feature_spec.pop(base.LABEL_KEY)
-
         parsed_features = tf.io.parse_example(serialized_tf_examples, feature_spec)
-
-        print("parsed_features: ", parsed_features)
-
         transformed_features = model.tft_layer(parsed_features)
-
-        print("transformed_features: ", transformed_features)
-        print("model(transformed_features): ", model(transformed_features))
 
         return {'outputs': model(transformed_features)}
 
@@ -69,7 +58,7 @@ def run_fn(fn_args: FnArgs):
         #print(raw_record[1].numpy())
 
     #serving_model_dir = "/".join(list(fn_args.serving_model_dir.split('/')[0:-1]))
-    serving_model_dir = fn_args.serving_model_dir
+    #serving_model_dir = fn_args.serving_model_dir
 
     model.fit(
             train_dataset,
@@ -80,43 +69,16 @@ def run_fn(fn_args: FnArgs):
             validation_steps=fn_args.eval_steps // batch_size,
             callbacks=[tensorboard_callback])
 
-    features_spec_sig = {
-        "examples": tf.TensorSpec(shape=[None], dtype=tf.string, name='examples'),
-        "image_floats": tf.TensorSpec(shape=[None, 784], dtype=tf.float32, name='image_floats')
-    }
+    #signatures = {
+    #        'serving_default': 
+    #            _get_serve_tf_examples_fn(
+    #                model, tf_transform_output).get_concrete_function(
+    #                    tf.TensorSpec(shape=[None], dtype=tf.string, name='examples')
+    #                )
+    #}
 
-    signatures = {
-            'serving_default': 
-                _get_serve_tf_examples_fn(
-                    model, tf_transform_output).get_concrete_function(
-                        #tf.TensorSpec(shape=((784, )), dtype=tf.float32, name='image_floats')
-                        tf.TensorSpec(shape=[None], dtype=tf.string, name='examples')
-                        #tf.TensorSpec(shape=[None, 784], dtype=tf.float32, name='image_floats')
-                    )
-    }
-
-    #tf.saved_model.save(model, fn_args.serving_model_dir, signatures=my_signatures)
     
-    model.export(serving_model_dir)
+    model.export(fn_args.serving_model_dir)
     #tf.saved_model.save(model, fn_args.serving_model_dir, signatures=signatures)
-
-    #signatures_new = base.make_serving_signatures(model, tf_transform_output)
-    #tf.saved_model.save(model, fn_args.serving_model_dir, signatures=signatures)
-    #tf.saved_model.save(model, fn_args.serving_model_dir, signatures=None)
-
-    #with tf.Graph().as_default() as new_graph:
-        #x = tf.compat.v1.placeholder(tf.float32, shape=[None, 784])
-        #y = tf.compat.v1.placeholder(tf.int64, shape=[None, 1])
-
-        #inputs = {"input": tf.compat.v1.saved_model.utils.build_tensor_info(x)}
-        #outputs = {"output": tf.compat.v1.saved_model.utils.build_tensor_info(y)}
-
-    #signatures_new = tf.compat.v1.saved_model.signature_def_utils.build_signature_def(
-    #        inputs=inputs,
-    #        outputs=outputs,
-    #        method_name=tf.compat.v1.saved_model.signature_constants.PREDICT_METHOD_NAME
-    #)
-
-    #tf.saved_model.save(model, fn_args.serving_model_dir, signatures=signatures_new)
 
 
